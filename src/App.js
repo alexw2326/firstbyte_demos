6a9d87d0f4327aabcb9060517342ee91fca68212
alexw2326 <alexw_19@icloud.com>
2025-01-19 19:15:25 -0500

    css demo

--- src/App.js
+++ src/App.js
@@ -2,46 +2,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="Website-Header">
+    <div className="app">
+      <header className="website-header">
         <title>Personal Website</title>
+        <link rel="stylesheet" href="App.css"></link>
       </header>
 
-      <body className="Website-Body">
-        <div className="Bio">
-          <h3>Your Name</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      <body className="website-body">
+        <div className="bio">
+          <h3 className="name-title">Your Name</h3>
+          <p className="bio-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
             deserunt mollit anim id est laborum.</p>
         </div>
-        <div className="Education">
-          <h4>Education</h4>
-          <ul>
+        <div className="education">
+          <h4 className="education-title">Education</h4>
+          <ul className="education-list">
             <li>Details of your education</li>
             <li>Potential second education</li>
           </ul>
         </div>
-        <div className="Work Experience">
-          <h4>Work Experience</h4>
-          <p>Details of first work experience in a paragraph</p>
-          <p>Details of second work experience in a paragraph</p>
+        <div className="work experience">
+          <h4 className="work-title">Work Experience</h4>
+          <p className="work1">Details of first work experience in a paragraph</p>
+          <p className="work2">Details of second work experience in a paragraph</p>
         </div>
-        <div className="Skills">
-          <h4>Skills</h4>
-            <ul>
+        <div className="skills">
+          <h4 className="skills-title">Skills</h4>
+            <ul className="skills-list">
               <li>Skill 1</li>
               <li>Skill 2</li>
               <li>Skill 3</li>
               <li>Skill 4</li>
             </ul>
         </div>
-        <div className="Projects">
-          <h4>Projects</h4>
-          <p>Details of project and description</p>
-          <p>Details of project and description</p>
+        <div className="projects">
+          <h4 className="projects-title">Projects</h4>
+          <p className="project1">Details of project and description</p>
+          <p className="project2">Details of project and description</p>
         </div>
       </body>
     </div>
